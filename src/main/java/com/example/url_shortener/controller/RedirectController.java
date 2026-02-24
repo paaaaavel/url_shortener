2ceba6682a1ce97ee23c1ae490ed0ec625f8dae3
konstantinlebedev <k.a.lebedev07@gmail.com>
2026-02-24 17:00:02 +0300

    clean

--- src/main/java/com/example/url_shortener/controller/RedirectController.java
+++ src/main/java/com/example/url_shortener/controller/RedirectController.java
@@ -2,9 +2,6 @@ package com.example.url_shortener.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.time.LocalDateTime;
-
 @RestController
 public class RedirectController
 {
