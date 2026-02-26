6bfa48ecc277acb4612776da4c15811e2679d0fb
Pavel <zuikin1616@gmail.com>
2026-02-26 22:07:54 +0300

    upd

--- src/main/java/com/example/url_shortener/controller/UrlController.java
+++ src/main/java/com/example/url_shortener/controller/UrlController.java
@@ -1,12 +1,16 @@
 package com.example.url_shortener.controller;
 
+import com.example.url_shortener.dto.CreateUrlRequest;
 import com.example.url_shortener.dto.UrlResponse;
 import com.example.url_shortener.service.UrlService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/urls")
 public class UrlController {
@@ -17,18 +21,28 @@ public class UrlController {
         this.urlService = urlService;
     }
 
+    @PostMapping
+    public ResponseEntity<UrlResponse> create(@Valid @RequestBody CreateUrlRequest request) {
+        log.info("HTTP create url");
+        UrlResponse created = urlService.createUrl(request);
+        return ResponseEntity.status(201).body(created);
+    }
+
     @GetMapping
     public List<UrlResponse> getAllUrls() {
+        log.info("HTTP get all urls");
         return urlService.getAllUrls();
     }
 
     @GetMapping("/{shortCode}")
     public UrlResponse getUrlByCode(@PathVariable String shortCode) {
+        log.info("HTTP get url by code={}", shortCode);
         return urlService.getUrlByCode(shortCode);
     }
 
     @DeleteMapping("/{shortCode}")
     public ResponseEntity<Void> deleteUrl(@PathVariable String shortCode) {
+        log.info("HTTP delete url code={}", shortCode);
         urlService.deleteUrl(shortCode);
         return ResponseEntity.noContent().build();
     }
